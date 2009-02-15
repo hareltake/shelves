138bcf8514136ec6a1152b03cea1cb15227fa689
romainguy@google.com <romainguy@google.com@a0f09db0-e1ff-11dd-8fb1-9960f2a117f8>
2009-02-15 08:31:51 +0000

    Fix formatting

--- trunk/Shelves/src/org/curiouscreature/android/shelves/scan/ScanIntent.java
+++ trunk/Shelves/src/org/curiouscreature/android/shelves/scan/ScanIntent.java
@@ -37,7 +37,8 @@ public final class ScanIntent {
     public static boolean isInstalled(Context context) {
         final PackageManager packageManager = context.getPackageManager();
         final Intent intent = new Intent(INTENT_ACTION_SCAN);
-        List<ResolveInfo> list = packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+        List<ResolveInfo> list = packageManager.queryIntentActivities(intent,
+                PackageManager.MATCH_DEFAULT_ONLY);
         return list.size() > 0;
     }
 }
